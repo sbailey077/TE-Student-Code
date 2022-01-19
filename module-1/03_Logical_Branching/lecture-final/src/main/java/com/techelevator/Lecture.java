3e8b13cce4696d171dca81aa6b71e7d1534d7ba0
Brian Lauvray <brian@techelevator.com>
2022-01-19 12:38:05 -0500

    Adding Logical Branching exercises and Loops and Arrays Tutorial

--- module-1/03_Logical_Branching/lecture-final/src/main/java/com/techelevator/Lecture.java
+++ module-1/03_Logical_Branching/lecture-final/src/main/java/com/techelevator/Lecture.java
@@ -6,7 +6,7 @@ public class Lecture {
     it so that it returns something other than a 1.
     */
     public int returnNotOne() {
-        return 1;
+        return 2;
     }
 
     /*
@@ -14,29 +14,31 @@ public class Lecture {
     it so that it returns something other than a 0.5.
     */
     public double returnNotHalf() {
-        return 0.5;
+        double returnValue = 0.2;
+        return returnValue;
     }
 
     /*
     3. This method needs to return a String. Fix it to return a valid String.
     */
     public String returnName() {
-        return null;
+        String name = "John Fulton";
+        return name;
     }
 
     /*
     4. This method currently returns an int. Change it so that it returns a double.
     */
-    public int returnDoubleOfTwo() {
-        return 2;
+    public double returnDoubleOfTwo() {
+        return 2.5;
     }
 
     /*
     5. This method should return the language that you're learning. Change
     it so that it does that.
     */
-    public boolean returnNameOfLanguage() {
-        return false;
+    public String returnNameOfLanguage() {
+        return "Java";
     }
 
     /*
@@ -45,7 +47,7 @@ public class Lecture {
     */
     public boolean returnTrueFromIf() {
         if (true) {
-            return false;
+            return true;
         }
 
         return false;
@@ -56,8 +58,9 @@ public class Lecture {
     to one. Make sure it returns true when one equals one.
     */
     public boolean returnTrueWhenOneEqualsOne() {
-        if (1 == 1) {
-            return false;
+        int x = 1;
+        if (x == 1) {
+            return true;
         }
 
         return false;
@@ -68,12 +71,14 @@ public class Lecture {
     greater than 5 and returns true if it is.
     */
     public boolean returnTrueWhenGreaterThanFive(int number) {
+        boolean isNumberGreaterThan5 = false;
+
         if (number > 5) {
-
+            isNumberGreaterThan5 = true;
         } else {
-
+            isNumberGreaterThan5 = false;
         }
-        return false;
+        return isNumberGreaterThan5;
     }
 
     /*
@@ -81,7 +86,7 @@ public class Lecture {
     How can we rewrite exercise 8 to have only one line of code?
     */
     public boolean returnTrueWhenGreaterThanFiveInOneLine(int number) {
-        return false; // What can we put here that returns a boolean that we want?
+        return number > 5; // What can we put here that returns a boolean that we want?
     }
 
     /*
@@ -91,23 +96,28 @@ public class Lecture {
     * We'll then return the result
     */
     public int returnNumberAfterAddThreeAndAddFive(int number, boolean addThree, boolean addFive) {
-        if (addThree) {
-            number = number + 1;
+        int newNumber = number;
+
+        // Check if addThree is TRUE, then number + 3
+        if (addThree == true) {
+            newNumber = newNumber + 3;
         }
 
-        // We can't use an else here. They could both be true, so we have to check each one.
-
-        if (addFive) {
-            number += 1;
+        // Check if addFive is TRUE, then number + 5
+        if (addFive == true) {
+            newNumber = newNumber + 5;
         }
-
-        return number;
+        // Return the new number
+        return newNumber;
     }
 
     /*
     11. Write an if statement that returns "Fizz" if the parameter is 3 and returns an empty String for anything else.
     */
     public String returnFizzIfThree(int number) {
+        if (number == 3) {
+            return "Fizz";
+        }
         return "";
     }
 
@@ -115,21 +125,30 @@ public class Lecture {
     12. Now write the above using the Ternary operator ?:. If you're not sure what this is, you can Google it.
     */
     public String returnFizzIfThreeUsingTernary(int number) {
-        return "";
+        return number == 3 ? "Fizz" : "";
     }
 
     /*
     13. Write an if/else statement that returns "Fizz" if the parameter is 3, "Buzz" if the parameter is 5 and an empty String for anything else.
     */
     public String returnFizzOrBuzzOrNothing(int number) {
-        return "";
+
+        if (number == 3) {
+            return "Fizz";
+        } else if (number == 5) {
+            return "Buzz";
+        } else {
+            return "";
+        }
+
     }
 
     /*
-    14. Write an if statement that checks if the parameter number is either equal to or greater than 18. Return "Adult" if it is or "Minor" if it's not.
+    14. Write an if statement that checks if the parameter number is either equal to or greater than 18.
+    Return "Adult" if it is or "Minor" if it's not.
     */
     public String returnAdultOrMinor(int number) {
-        if (true) {
+        if (number >= 18) {
             return "Adult";
         } else {
             return "Minor";
@@ -137,27 +156,30 @@ public class Lecture {
     }
 
     /*
-    15. Now, do it again with a different boolean opeation.
+    15. Now, do it again with a different boolean operation.
     */
     public String returnAdultOrMinorAgain(int number) {
-        if (true) {
+        if (!(number < 18)) {
             return "Adult";
         } else {
             return "Minor";
         }
     }
+
 
     /*
     16. Return as above, but also return "Teen" if the number is between 13 and 17 inclusive.
     */
     public String returnAdultOrMinorOrTeen(int number) {
-        if (true) {
+        if (number > 17) {
             return "Adult";
-        } else if (true) {
+        } else if (number >= 13) {
             return "Teen";
         } else {
             return "Minor";
         }
+
+        //if (number >= 13 && number <= 17)
     }
 
 }
