5e6683c9fadc151a85cda9ee6feec8a3c579de57
Brian Lauvray <brian@techelevator.com>
2022-01-21 12:13:16 -0500

    notes for MakeChange

--- module-1/05_Command_Line_Programs/extra-project-final/src/main/java/com/techelevator/MakeChange.java
+++ module-1/05_Command_Line_Programs/extra-project-final/src/main/java/com/techelevator/MakeChange.java
@@ -4,7 +4,7 @@ package com.techelevator;
  Write a command line program which prompts the user for the total bill, and the amount tendered. It should then
  display the change required.
 
- $ java MakeChange
+
  Please enter the amount of the bill: 23.65
  Please enter the amount tendered: 100.00
  The change required is 76.35
@@ -13,6 +13,15 @@ public class MakeChange {
 
 	public static void main(String[] args) {
 
+		/*
+			1. Create a Scanner with System.in
+			2. Ask the user the amount of the bill
+			3. Get the users response and store it as a double
+			4. Ask the user the amount tendered
+			5. Get the users response and store it as a double
+			6. calculate change = tenderedAmount - amountOfBill
+			7. print the change to the user
+		 */
 	}
 
 }
