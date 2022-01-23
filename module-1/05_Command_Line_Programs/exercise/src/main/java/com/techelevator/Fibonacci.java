9d5e86ed9b91db7f3933499598eb1be48a5bd0ff
Sam Bailey <s.bailey077@gmail.com>
2022-01-22 20:54:33 -0500

    Committing Fibonnaci Sequence

--- module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
@@ -1,8 +1,56 @@
 package com.techelevator;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Fibonacci {
 
 	public static void main(String[] args) {
+
+		Scanner userInput = new Scanner(System.in);
+
+		//Gather user input
+
+		System.out.print("Please enter a number: ");
+		String userEnteredString = userInput.nextLine();
+
+		//Parse user input into an integer
+
+		int enteredNumber = Integer.parseInt(userEnteredString);
+
+		//Initialize the first two terms of the series
+
+		int firstTerm = 0;
+		int secondTerm = 1;
+
+		//Create a for loop to iterate through the series
+
+		for (int i = 0; firstTerm <= enteredNumber; i++) {
+
+			//Print the first term and a space for the next term
+
+			System.out.print(firstTerm + " ");
+
+			//Create a variable for the next term
+
+			int nextTerm = firstTerm + secondTerm;
+
+			//Change the first term and second term once they have already printed
+
+			firstTerm = secondTerm;
+			secondTerm = nextTerm;
+
+		}
+
+
+
+
+
+
+
+
+
+
 
 	}
 
