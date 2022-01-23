d1558ba76db55c2b3728cbc6a38dd9d520b7c9f4
Sam Bailey <s.bailey077@gmail.com>
2022-01-23 15:33:47 -0500

    Committing Module 1 Exercise 5

--- module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
+++ module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
@@ -1,8 +1,38 @@
 package com.techelevator;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class DecimalToBinary {
 
 	public static void main(String[] args) {
+
+		//Import new scanner
+
+		Scanner userInput = new Scanner(System.in);
+
+		//Take user input
+		System.out.print("Please enter in a series of decimal integer values (separated by spaces): ");
+		String userEnteredDecimals = userInput.nextLine();
+
+		System.out.println();
+
+		String[] arrayOfEnteredDecimals = userEnteredDecimals.split(" ");
+		int[] stringToInteger = new int[arrayOfEnteredDecimals.length];
+
+		for (int i = 0; i < arrayOfEnteredDecimals.length; i++) {
+			stringToInteger[i] = Integer.parseInt(arrayOfEnteredDecimals[i]);
+		}
+
+		for (int i = 0; i < stringToInteger.length; i++){
+			System.out.println(stringToInteger[i] + " in binary is " + Integer.toBinaryString(stringToInteger[i]));
+		}
+
+
+
+
+
 
 	}
 
