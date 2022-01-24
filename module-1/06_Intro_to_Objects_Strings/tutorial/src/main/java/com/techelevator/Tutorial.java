8ca75d26009b8bdd0b47eea721ffbe21b9ff0113
Sam Bailey <s.bailey077@gmail.com>
2022-01-24 08:55:34 -0500

    Committing to pull

--- module-1/06_Intro_to_Objects_Strings/tutorial/src/main/java/com/techelevator/Tutorial.java
+++ module-1/06_Intro_to_Objects_Strings/tutorial/src/main/java/com/techelevator/Tutorial.java
@@ -8,13 +8,31 @@ public class Tutorial {
 
         // ***********  Step 1: Use the *new* operator to create Strings on the Heap  *************
 
+        // Create a new string from an array of characters
 
+        char[] helloChars = new char[] {'H', 'e', 'l', 'l', 'o', '!'};
+        String greeting = new String(helloChars);
+        System.out.println("Greeting: " + greeting);
 
+        //You can also create a string by passing in a literal value, in double-quotes
+
+        String salutation = new String("Welcome my friend");
+        System.out.println("Salutation: " + salutation);
+
+        //Java allows you to skip the *new* operator when creating a new String
+
+        String toast = "May the compiler rise up to meet you.";
+        System.out.println("Toast: " + toast);
 
 
         // ***********  Step 2: Try out some String methods  *************
 
+        //Prompt the user to enter a sentence
 
+        System.out.println("Please type a sentence: ");
+        Scanner scanner = new Scanner(System.in);
+        String sentence = scanner.nextLine();
+        System.out.println(sentence);
 
 
 
