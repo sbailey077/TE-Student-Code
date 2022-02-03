afe67f4acae0ab60576e47ca4841a39ddd98eb58
Sam Bailey <s.bailey077@gmail.com>
2022-02-03 13:16:56 -0500

    Committing to pull

---
+++ module-1/14_Unit_Testing/exercise/src/test/java/com/techelevator/SameFirstLastTest.java
@@ -0,0 +1,26 @@
+package com.techelevator;
+
+import org.junit.*;
+
+public class SameFirstLastTest {
+
+
+    @Before
+    public void setup () {
+        
+    }
+
+    /*
+        Things to Test
+        IsItTheSame ([1, 2, 3]) -> false
+        IsItTheSame ([1, 2, 3, 1]) -> true
+        IsItTheSame ([1, 2, 1]) -> true
+        IsItTheSame ([-1, -2, -1]) -> true
+        IsItTheSame ([null]) -> false
+        IsItTheSame ([]) -> false
+        IsItTheSame ([1]) -> true
+
+     */
+
+
+}
