dc04e9001fdb18b19080db0164a56c4f44f683d9
Sam Bailey <s.bailey077@gmail.com>
2022-02-07 15:13:32 -0500

    Committing to pull

--- module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
+++ module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
@@ -1,8 +1,21 @@
 package com.techelevator;
 
+
+import java.util.Scanner;
+
 public class WordSearch {
 
 	public static void main(String[] args) {
+
+		System.out.println("What is the name of the file that should be searched?");
+
+		Scanner userInput = new Scanner(System.in);
+
+		
+
+
+		File file = new File(path);
+
 
 	}
 
