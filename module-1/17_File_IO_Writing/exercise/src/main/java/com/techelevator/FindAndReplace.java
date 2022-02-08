e53aac0b42a6a92f3c35286e855230e7515338b9
Sam Bailey <s.bailey077@gmail.com>
2022-02-08 14:08:03 -0500

    Committing to pull

--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,8 +1,25 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class FindAndReplace {
 
     public static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("What is the search word?");
+        String searchWord = userInput.nextLine();
+
+        System.out.println("What is the replacement word?");
+        String replacementWord = userInput.nextLine();
+
+        System.out.println("What is the source file?");
+        String sourceFile = userInput.nextLine();
+
+        System.out.println("What is the destination file?");
+        String destinationFile = userInput.nextLine();
+
+
 
     }
 
