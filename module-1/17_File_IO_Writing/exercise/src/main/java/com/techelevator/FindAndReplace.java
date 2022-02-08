b266ffd2042000bc3fd62d3b031dae000cf448b9
Sam Bailey <s.bailey077@gmail.com>
2022-02-08 16:33:27 -0500

    Committing exercise

--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,5 +1,9 @@
 package com.techelevator;
 
+import java.io.*;
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class FindAndReplace {
@@ -19,8 +23,29 @@ public class FindAndReplace {
         System.out.println("What is the destination file?");
         String destinationFile = userInput.nextLine();
 
-
-
+        File file = new File(sourceFile);
+        List<String> linesToPrint = new ArrayList<String>();
+        try(Scanner fileScanner = new Scanner(file)) {
+            while(fileScanner.hasNextLine()) {
+                String replacementLine = "";
+                String line = fileScanner.nextLine();
+                if (line.contains(searchWord)) {
+                    replacementLine = line.replace(searchWord, replacementWord);
+                    linesToPrint.add(replacementLine);
+                } else {
+                    linesToPrint.add(line);
+                }
+            } try (PrintWriter printWriter = new PrintWriter(destinationFile);
+                   BufferedWriter bufferedWriter = new BufferedWriter(printWriter)) {
+                    for (String printedLine : linesToPrint) {
+                        bufferedWriter.write(printedLine);
+                        bufferedWriter. newLine();
+                    }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File " + sourceFile + " not found.");
+        } catch (IOException e) {
+            System.out.println("Unexpected error occurred.");
+        }
     }
-
 }
--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
@@ -1,9 +1,44 @@
 package com.techelevator;
 
+import javax.imageio.IIOException;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class FizzWriter {
 
 	public static void main(String[] args) {
 
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Please enter the destination file:");
+		String destinationFile = userInput.nextLine();
+
+		List<String> intList = new ArrayList<String>();
+		for (int i = 1; i <= 300; i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				intList.add("FizzBuzz");
+			} else if (i % 3 == 0) {
+				intList.add("Fizz");
+			} else if (i % 5 == 0) {
+				intList.add("Buzz");
+			} else {
+				intList.add(String.valueOf(i));
+			}
+		}
+
+		try(PrintWriter printWriter = new PrintWriter(destinationFile);
+			BufferedWriter bufferedWriter = new BufferedWriter(printWriter)) {
+			for (String s : intList ) {
+				bufferedWriter.write(s);
+				bufferedWriter.newLine();
+			}
+		} catch(IOException e) {
+			System.out.println("Unexpected error occurred.");
+		}
 	}
 
 }
