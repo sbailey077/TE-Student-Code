6691ea6f7c6078a80f634f0c4a6b9b3abee0e5c0
Brian Lauvray <brian@techelevator.com>
2022-02-09 16:48:29 -0500

    Review code

--- module-1/18_Review/lecture-final/src/main/java/com/techelevator/Application.java
+++ module-1/18_Review/lecture-final/src/main/java/com/techelevator/Application.java
@@ -3,46 +3,45 @@ package com.techelevator;
 import com.techelevator.inventory.Inventory;
 import com.techelevator.inventory.Item;
 
+import java.io.FileNotFoundException;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Application {
 
-    /*
-        The Store
-            Holds the Inventory
-        Getting User Input
-        Workflow
+    private Menu menu;
 
-     */
     public static void main(String[] args) {
+        Application app = new Application();
+        app.run();
+    }
 
-        Scanner userInput = new Scanner(System.in);
-        Inventory inventoryBuilder = new Inventory();
-        System.out.println("Welcome to Java Blue Mart");
-        System.out.println();
+    public void run() {
+        menu = new Menu();
+
+        Inventory inventoryBuilder = null;
+        boolean hasFileLocation = false;
+        while(!hasFileLocation) {
+            String inventoryPath = menu.getInputFromUser("Path to inventory file: ");
+            try {
+                inventoryBuilder = new Inventory(inventoryPath);
+                hasFileLocation = true;
+            } catch (FileNotFoundException e) {
+                menu.displayError("File not found.");
+            }
+        }
+
+        menu.showWelcomeMessage();
+  
 
         Map<String, Item> inventory = inventoryBuilder.getInventory();
 
-        System.out.println("Items for Sale");
+        menu.showItemsForSale(inventory);
 
-        for ( Map.Entry<String, Item> mapEntry : inventory.entrySet()) {
-
-            System.out.print( mapEntry.getValue().getSku() );
-            System.out.print( " : " + mapEntry.getValue().getName() );
-            System.out.print( " ( " + mapEntry.getValue().getDescription() + " ) ");
-            System.out.println( " $" + mapEntry.getValue().getTotalPrice() );
-
-        }
-
-        System.out.println();
-        System.out.print("Item to purchase >>>");
-        String skuToPurchase = userInput.nextLine();
-
+        String skuToPurchase = menu.getSelectedSkuFromUser();
         Item selectedItem = inventory.get(skuToPurchase);
 
-        System.out.println("You selected to purchase a: " + selectedItem.getName());
-
+        menu.showUserSelectedItem(selectedItem);
 
     }
     
--- module-1/18_Review/lecture-final/src/main/java/com/techelevator/inventory/Inventory.java
+++ module-1/18_Review/lecture-final/src/main/java/com/techelevator/inventory/Inventory.java
@@ -2,41 +2,65 @@ package com.techelevator.inventory;
 
 import com.techelevator.inventory.Item;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Inventory {
 
+    private String inventoryFilePath;
+    private Map<String, Item> inventory = new HashMap<String, Item>();
+
+    public Inventory(String inventoryFilePath) throws FileNotFoundException {
+        this.inventoryFilePath = inventoryFilePath;
+        loadInventoryFromFile();
+    }
+
     public Map<String, Item> getInventory() {
-        Item cake = new Food("001", "Cake");
-        cake.setDescription("A chocolate cake");
-        cake.setPerishable(true);
-        cake.setPrice(10);
+        return this.inventory;
+    }
 
-        Item flipflops = new Clothing("002", "Flip Flop Sandals");
-        flipflops.setDescription("Protect your feet, but only the bottoms");
-        flipflops.setPerishable(false);
-        flipflops.setPrice(7);
+    private void loadInventoryFromFile() throws FileNotFoundException {
+        File file = new File(inventoryFilePath);
+        try (Scanner inventoryScanner = new Scanner(file)) {
+            while (inventoryScanner.hasNextLine()) {
+                String line = inventoryScanner.nextLine();
+                Item newItem = buildItemFromLine(line);
+                inventory.put(newItem.getSku(), newItem);
+            }
+        }
+    }
 
-        Item cat = new Pet("cat01", "Cat", "Evil Bob", 10);
-        cat.setDescription("Covered in fur");
-        cat.setPerishable(true);
-        cat.setPrice(100);
+    private Item buildItemFromLine(String line) {
+        Item item = null;
+        String[] parts = line.split("\\|");
 
-        Item frog = new Pet("f22", "Frog", 25);
-        frog.setDescription("Greenish and not a toad");
-        frog.setPerishable(true);
-        frog.setPrice(5.25);
+        String sku = parts[0];
+        String name = parts[1];
+        String description = parts[2];
+        String perishable = parts[3];
+        String price = parts[4];
+        String type = parts[5];
+
+        if (type.equalsIgnoreCase("F")) {
+           item = new Food(sku, name);
+        } else if (type.equalsIgnoreCase("C")) {
+           item = new Clothing(sku, name);
+        } else if (type.equalsIgnoreCase("B")) {
+            item = new Book(sku, name);
+        } else if (type.equalsIgnoreCase("P")) {
+            String weight = parts[6];
+            String petName = parts[7];
+            item = new Pet(sku, name, petName, Integer.parseInt(weight));
+        }
 
 
-        Map<String, Item> inventory = new HashMap<String, Item>();
-
-        inventory.put(cake.getSku(), cake);
-        inventory.put(flipflops.getSku(), flipflops);
-        inventory.put(cat.getSku(), cat);
-        inventory.put(frog.getSku(), frog);
-        inventory.put("B1", new Book("B1", "Head First Java"));
-
-        return inventory;
+        item.setDescription(description);
+        item.setPerishable( perishable.equalsIgnoreCase("Y") );
+        item.setPrice(Double.parseDouble(price));
+        return item;
     }
 }
