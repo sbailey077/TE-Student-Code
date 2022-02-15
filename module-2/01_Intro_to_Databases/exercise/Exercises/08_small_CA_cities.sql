d703d5da109e3cd469ac53b02aeebfa9bbf6e6c9
Sam Bailey <s.bailey077@gmail.com>
2022-02-15 12:19:43 -0500

    Committing exercise

--- module-2/01_Intro_to_Databases/exercise/Exercises/08_small_CA_cities.sql
+++ module-2/01_Intro_to_Databases/exercise/Exercises/08_small_CA_cities.sql
@@ -1,2 +1,4 @@
 -- 8. The name and population of cities in California (CA) with a population less than 150,000 people (37 rows)
-
+SELECT city_name, population
+FROM city
+WHERE population < 150000 AND state_abbreviation = 'CA';
--- module-2/01_Intro_to_Databases/lecture/lecture-queries.sql
+++ module-2/01_Intro_to_Databases/lecture/lecture-queries.sql
@@ -23,43 +23,60 @@
 -- Write queries to retrieve...
 
 -- The names of cities in California (CA)
-
+SELECT city_name
+FROM city 
+WHERE state_abbreviation = 'CA';
 
 -- The names and state abbreviations of cities NOT in California
-
+SELECT city_name, state_abbreviation
+FROM city
+WHERE state_abbreviation != 'CA';
 
 -- The names and areas of cities smaller than 25 square kilometers 
-
+SELECT city_name, area
+FROM city
+WHERE area < 25;
 
 -- The names from all records in the state table that have no assigned census region
-
+SELECT * 
+FROM state
+WHERE census_region IS null;
 
 -- The names and census regions from all records in the state table that have an assigned census region
-
-
+SELECT * 
+FROM state
+WHERE census_region IS NOT null;
 
 -- WHERE with AND/OR
 -- Write queries to retrieve...
 
 -- The names, areas, and populations of cities smaller than 25 sq. km. with more than 100,000 people
-
+SELECT city_name, area, population
+FROM city
+WHERE area < 25 AND population > 100000;
 
 -- The names and census regions of all states (and territories and districts) not in the Midwest region (including those that don't have any census region)
-
+SELECT state_name, census_region
+FROM state
+WHERE census_region != 'Midwest' OR census_region IS null;
 
 -- The names, areas, and populations of cities in California (CA) or Florida (FL)
-
+select city_name, area, population
+FROM city
+where state_abbreviation = 'CA' or state_abbreviation = 'FL';
 
 -- The names, areas, and populations of cities in New England -- Connecticut (CT), Maine (ME), Massachusetts (MA), New Hampshire (NH), Rhode Island (RI) and Vermont (VT)
-
-
+select city_name, area, population
+FROM city
+where state_abbreviation IN ('CT', 'ME', 'MA', 'NH', 'RI', 'VT');
 
 -- SELECT statements involving math
 -- Write a query to retrieve the names and areas of all parks in square METERS
 -- (the values in the database are stored in square kilometers)
 -- Label the second column "area_in_meters"
-
-
+ 
+select park_name, (area * 1000) as area_in_meters
+from park;
 
 -- All values vs. distinct values
 
