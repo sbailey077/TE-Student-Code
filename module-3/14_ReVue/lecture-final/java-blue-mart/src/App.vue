70ecbe845885a9fe64a85fab52538f4b9f453f44
Brian Lauvray <brian@techelevator.com>
2022-03-31 17:17:34 -0400

    Added ReVue lecture

---
+++ module-3/14_ReVue/lecture-final/java-blue-mart/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h1>Welcome to Java Blue Mart</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+
+  },
+   mounted() {
+   this.$store.dispatch("getProducts");
+ }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
